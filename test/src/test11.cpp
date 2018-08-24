6cb72bd79a48db5c05e8ab9f74f13519e43859b7
Nagavenkat Adurthi <n.adurthi@gmail.com>
2018-08-24 01:21:58 -0500

    run python code

--- test/src/test11.cpp
+++ test/src/test11.cpp
@@ -2,7 +2,24 @@
 #include "myproject/base.h"
 
 
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <array>
 
+std::string exec(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    while (!feof(pipe.get())) {
+        if (fgets(buffer.data(), 128, pipe.get()) != nullptr)
+            result += buffer.data();
+    }
+    return result;
+}
 
 
 int main(int argc, char * argv[]){
@@ -10,5 +27,7 @@ int main(int argc, char * argv[]){
 	V v;
 	div(v);
 	std::cout << "test ok \n";
+
+	exec("python -c \"import pandas as pd;import numpy as np;df=pd.DataFrame();print df;import matplotlib.pyplot as plt;plt.plot([1,2],[1,2]);plt.show();\"");
 
 }
